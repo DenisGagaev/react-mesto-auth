07240f603cc47b75ee83cb285aecc0f4cbb1e03c
DenisGagaev <Shaker8@yandex.ru>
2021-10-14 22:32:46 +0300

    Рефакторинг

--- src/components/card.js
+++ src/components/card.js
@@ -2,10 +2,10 @@ import React from "react";
 
 function Card(props) {
 
-  const { card, likeCounter, onCardClick } = props
+  const { card } = props
 
   function handleClick() {
-    onCardClick(card);
+    props.onCardClick(card);
   }
 
   return (
@@ -16,7 +16,7 @@ function Card(props) {
         <h2 className="element__text">{card.name}</h2>
         <div className="element__like-container">
           <button className="element__like" type="button"></button>
-          <span className="element__like-counter">{likeCounter}</span>
+          <span className="element__like-counter">{props.likeCounter}</span>
         </div>
         <button className="elements__delete" type="button"></button>
       </div>
